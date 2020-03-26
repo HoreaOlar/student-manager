a02632ea60281be7cd3982a7e337b885aa636f80
Raluca <raluca.borca09@gmail.com>
2020-03-26 11:28:40 +0200

    SM-4 Added kitschy styling for login screen

--- src/loose/oose/fis/lab/student/manager/model/Student.java
+++ src/loose/oose/fis/lab/student/manager/model/Student.java
@@ -40,4 +40,36 @@ public class Student {
     public int hashCode() {
         return Objects.hash(firstName, lastName, age, meanGrade);
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getMeanGrade() {
+        return meanGrade;
+    }
+
+    public void setMeanGrade(double meanGrade) {
+        this.meanGrade = meanGrade;
+    }
 }
